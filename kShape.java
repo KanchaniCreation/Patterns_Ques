b5e0e99422d5a802ae9cb6eb50aca52471d4ca73
KanchaniCreation <hkanchanigupta@gmail.com>
2025-09-01 21:47:45 +0530

    update

--- kShape.java
+++ kShape.java
@@ -1,15 +1,15 @@
 public class kShape {
     public static void main(String[] args){
-
+int a=65;
         for(int i =4;i>=1;i--){
             for (int j=1;j<=i;j++) {
-                System.out.print(j);
+                System.out.print((char)(a+j)+"");
             }
             System.out.println();
         }
         for (int i =1;i<5;i++){
             for (int j=1;j<=i;j++){
-            System.out.print(j);
+            System.out.print((char)(a+j)+"");
         }
         System.out.println();
     }
