ba2a44a97796aeafd4adc93f26456ebfb159abdc
KanchaniCreation <hkanchanigupta@gmail.com>
2025-09-02 16:17:36 +0530

    update

--- fibo.java
+++ fibo.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
 public class fibo {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your number ");
+        int num = sc.nextInt();
         int first = 0,second=1;
-        for (int i=0;i<=9;i++){
+        for (int i=1;i<=num;i++){
             
       int fib=first+second;
        System.out.println(first);
